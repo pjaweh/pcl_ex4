8bda5f9340f4be4daa6beb7e5851150f46ff3893
pjaweh <patrick.hlr@gmail.com>
2017-04-17 11:17:31 +0200

    implemented getfreqwords method correctly, excluded sentences from picture caption sections

--- ex01/ex01.py
+++ ex01/ex01.py
@@ -9,41 +9,52 @@ import lxml.etree as ET
 import glob
 import operator
 
+
 def main():
-    mydict = {}
-    files = [file for file in glob.glob('SAC/SAC-Jahrbuch*_mul.xml')]
-    #print(files)
-    for file in files:
-        for entry in extract(file):
-            if len(entry.split()) >= 6:
-                if entry in mydict:
-                    mydict[entry] += 1
-                else:
-                    mydict[entry] = 1
-    sorted_x = sorted(mydict.items(), key=operator.itemgetter(1), reverse=True)
-    outfile = open('outfile.txt', 'w')
-    for k,v in sorted_x:
-        outfile.write('{} \t {} \n'.format(v,k))
-    outfile.close()
+    try:
+        getfreqwords('SAC', 'ouftile.txt')
+    except MemoryError:
+        print('Too much memory used. Optimize your program.')
 
 
 def extract(file):
-    '''Get lemma sentences 
-    '''
+    """Get lemmatized sentences 
+    """
     for _, sentence in ET.iterparse(file, tag='s'):
-        sent = ''
-        for word in sentence.iterfind('.//w'):
-            # check if attribute exists
-            if 'lemma' in word.attrib:
-                sent = '{} {}'.format(sent,word.attrib['lemma'])
-        yield sent
+        # ignore sentences in picture captions
+        if sentence.getparent().tag != 'caption':
+            sent = ''
+            for word in sentence.iterfind('.//w'):
+                # check if attribute exists
+                if 'lemma' in word.attrib:
+                    sent = '{} {}'.format(sent,word.attrib['lemma'])
+                word.clear()
+            yield sent
         sentence.clear()
 
 def getfreqwords(indir, outfile):
-    pass
-    # indir = Verzeichnis mit XML-Daten
-    # outfile = Textdatei für Ausgabe
-    # durch alle XML-Dateien vom Schema ’SAC-Jahrbuch_XXXX_mul.xml’ durchgehen
+    """Get 20 most frequent lemmatized sentences
+    from a collection of xml files (indir) and write
+    them to a textfile (outfile)
+    """
+    lemma_dict = {}
+    files = [file for file in glob.glob(
+        '{}/{}'.format(indir, 'SAC-Jahrbuch*_mul.xml')
+        )]
+    for file in files:
+        for entry in extract(file):
+            if len(entry.split()) >= 6:
+                if entry in lemma_dict:
+                    lemma_dict[entry] += 1
+                else:
+                    lemma_dict[entry] = 1
+    lemma_dict_sorted = sorted(
+        lemma_dict.items(), key=operator.itemgetter(1), reverse=True
+        )
+    outfile = open(outfile, 'w')
+    for k,v in lemma_dict_sorted[0:20]:
+        outfile.write('{} \t {} \n'.format(v,k))
+    outfile.close()
 
 
 if __name__ == '__main__':
